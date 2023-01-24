7489752c4eb5b2006a19e18ac7efada00967c46f
Hadi Cahyono <hadi@Hadis-MacBook-Air.local>
2023-01-24 22:23:36 +0800

    register & login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,124 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { API_URL } from "../../helper";
+import {
+  Flex,
+  Box,
+  FormControl,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const Login = (props) => {
-  return <>Login</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post(API_URL + `/users/login`, {
+        email,
+        password,
+      });
+
+      if (data.success) {
+        alert(data.message);
+        window.location = "/";
+      } else {
+        setError(data.message);
+        console.log(error);
+      }
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+          <Text>{error && <p>{error}</p>}</Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </FormControl>
+            <FormControl id="password">
+              <Input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: "column", sm: "row" }}
+                align={"start"}
+                justify={"space-between"}
+              >
+                <Checkbox>Remember me</Checkbox>
+                <Link color={"blue.400"}>Forgot password?</Link>
+              </Stack>
+              <Button
+                onClick={handleSubmit}
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+
+    // <form onSubmit={handleSubmit}>
+    //   <input
+    //     type="email"
+    //     value={email}
+    //     onChange={(e) => setEmail(e.target.value)}
+    //     placeholder="Email"
+    //     required
+    //   />
+    //   <input
+    //     type="password"
+    //     value={password}
+    //     onChange={(e) => setPassword(e.target.value)}
+    //     placeholder="Password"
+    //     required
+    //   />
+    //   <button type="submit">Login</button>
+    //   {error && <p>{error}</p>}
+    // </form>
+  );
 };
 
 export default Login;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,22 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { API_URL } from "../../helper";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+// import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 const Register = (props) => {
   const [email, setEmail] = useState("");
@@ -8,6 +24,7 @@ const Register = (props) => {
   const [passwordAgain, setPasswordAgain] = useState("");
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,44 +44,139 @@ const Register = (props) => {
       }
     } catch (error) {
       console.error(error);
-      setError("An error occured while registering.");
+      setError(error.response.data.message);
     }
   };
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Confirm Password:
-          <input
-            type="password"
-            value={passwordAgain}
-            onChange={(e) => setPasswordAgain(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Register</button>
-      </form>
-      {error && <p>{error}</p>}
-      {success && <p>Account registered successfully!</p>}
-    </>
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Register
+          </Heading>
+          <Text>
+            {error && <p>{error}</p>}
+            {success && <p>Account registered successfully!</p>}
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {/* {showPassword ? <ViewIcon /> : <ViewOffIcon />} */}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Confirm Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  value={passwordAgain}
+                  onChange={(e) => setPasswordAgain(e.target.value)}
+                />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {/* {showPassword ? <ViewIcon /> : <ViewOffIcon />} */}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                onClick={handleSubmit}
+                loadingText="Submitting"
+                size="lg"
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Register
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user? <Link color={"blue.400"}>Login</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+
+    // <>
+    //   <form onSubmit={handleSubmit}>
+    //     <label>
+    //       Email:
+    //       <input
+    //         type="email"
+    //         value={email}
+    //         onChange={(e) => setEmail(e.target.value)}
+    //       />
+    //     </label>
+    //     <br />
+    //     <label>
+    //       Password:
+    //       <input
+    //         type="password"
+    //         value={password}
+    //         onChange={(e) => setPassword(e.target.value)}
+    //       />
+    //     </label>
+    //     <br />
+    //     <label>
+    //       Confirm Password:
+    //       <input
+    //         type="password"
+    //         value={passwordAgain}
+    //         onChange={(e) => setPasswordAgain(e.target.value)}
+    //       />
+    //     </label>
+    //     <br />
+    //     <button type="submit">Register</button>
+    //   </form>
+    //   {error && <p>{error}</p>}
+    //   {success && <p>Account registered successfully!</p>}
+    // </>
   );
 };
 
